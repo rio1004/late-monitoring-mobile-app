dd373a27ac24c9ac78f9e44f8868f62be8b05225
7iquidKtc <tavlegktc@gmail.com>
2025-01-11 17:41:52 +0800

    up 3 navigation bar from figma

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -9,16 +9,20 @@ import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
+
   'house.fill': 'home',
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
-} as Partial<
-  Record<
-    import('expo-symbols').SymbolViewProps['name'],
-    React.ComponentProps<typeof MaterialIcons>['name']
-  >
->;
+  'history': 'history',
+  'profile': 'manage-accounts',
+} 
+// as Partial<
+//   Record<
+//     import('expo-symbols').SymbolViewProps['name'],
+//     React.ComponentProps<typeof MaterialIcons>['name']
+//   >
+// >;
 
 export type IconSymbolName = keyof typeof MAPPING;
 
