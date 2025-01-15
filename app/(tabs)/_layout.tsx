18cae4e8fa4327f148d876951cc3af8138ed03f2
7iquid <taviefalcon@yahoo.com>
2025-01-16 00:21:13 +0800

    add header tab, logo and notif icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,45 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Image, Platform, View } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorSchemeMod";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme];
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+        headerShown: true, // Enable headers globally
+        headerStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].background,
+          borderBottomWidth: 0,
+        },
+        // headerTintColor: Colors[colorScheme ?? "light"].text,
+        headerTitle: () => (
+          <View style={{ flexDirection: "row", }}>
+            <Image
+              source={require("@/assets/logo.png")} // Use your image path here
+              style={{ width: 90, height: 40 }}
+            />
+            <IconSymbol
+              size={40}
+              name="notification"
+              color={theme.tabIconDefault}
+            />
+          </View>
+        ),
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: "absolute",
           },
           default: {},
@@ -30,7 +49,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="history"
         options={{
-          title: 'History',
+          title: "History",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="history" color={color} />
           ),
--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -1,22 +1,25 @@
 // This file is a fallback for using MaterialIcons on Android and web.
 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight } from 'expo-symbols';
-import React from 'react';
-import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { SymbolWeight } from "expo-symbols";
+import React from "react";
+import { OpaqueColorValue, StyleProp, ViewStyle } from "react-native";
 
 // Add your SFSymbol to MaterialIcons mappings here.
 const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
 
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
-  'history': 'history',
-  'profile': 'manage-accounts',
-} 
+  "house.fill": "home",
+  "paperplane.fill": "send",
+  "chevron.left.forwardslash.chevron.right": "code",
+  "chevron.right": "chevron-right",
+  history: "history",
+  profile: "manage-accounts",
+  notification: "notifications-none",
+};
+
+{/* <MaterialIcons name="notifications-none" size={24} color="black" /> */}
 // as Partial<
 //   Record<
 //     import('expo-symbols').SymbolViewProps['name'],
@@ -43,5 +46,12 @@ export function IconSymbol({
   style?: StyleProp<ViewStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return (
+    <MaterialIcons
+      color={color}
+      size={size}
+      name={MAPPING[name]}
+      style={style}
+    />
+  );
 }
